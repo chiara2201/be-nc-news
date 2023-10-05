75995ef3609a0e67c98454bac8ba37642d03b313
chiara2201 <71043804+chiara2201@users.noreply.github.com>
2023-10-05 15:50:42 +0100

    created filter articles by topic

--- models/models.js
+++ models/models.js
@@ -7,25 +7,46 @@ exports.fetchTopics = () => {
 }
 
 exports.fetchArticles = (topic) => {
-	const query = `
-	SELECT 
-		articles.author, 
-		articles.title, 
-		articles.article_id, 
-		articles.topic, 
-		articles.created_at,
-		articles.votes, 
-		articles.article_img_url,
-		COUNT(comments.article_id) as comment_count 
-	FROM articles 
-	LEFT JOIN comments ON articles.article_id = comments.article_id 
-	WHERE topic = $1
-	GROUP BY articles.article_id 
-	ORDER BY articles.created_at DESC;`
+	if (topic) {
+		const query = `
+		SELECT 
+			articles.author, 
+			articles.title, 
+			articles.article_id, 
+			articles.topic, 
+			articles.created_at,
+			articles.votes, 
+			articles.article_img_url,
+			COUNT(comments.article_id) as comment_count 
+		FROM articles 
+		LEFT JOIN comments ON articles.article_id = comments.article_id 
+		WHERE topic = $1
+		GROUP BY articles.article_id 
+		ORDER BY articles.created_at DESC;`
 
-	return db.query(query, [topic]).then((result) => {
-		return result.rows
-	})
+		return db.query(query, [topic]).then((result) => {
+			return result.rows
+		})
+	} else {
+		const query = `
+		SELECT 
+			articles.author, 
+			articles.title, 
+			articles.article_id, 
+			articles.topic, 
+			articles.created_at,
+			articles.votes, 
+			articles.article_img_url,
+			COUNT(comments.article_id) as comment_count 
+		FROM articles 
+		LEFT JOIN comments ON articles.article_id = comments.article_id 
+		GROUP BY articles.article_id 
+		ORDER BY articles.created_at DESC;`
+
+		return db.query(query).then((result) => {
+			return result.rows
+		})
+	}
 }
 
 exports.fetchArticleById = (articleId) => {
